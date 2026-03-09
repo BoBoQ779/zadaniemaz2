a28e6ae424a84a9cacbce241ca3fea89e584747f
BoBoQ779 <bartoshziemniak@op.pl>
2026-03-09 08:49:22 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,11 @@
-public class Main {
+import java.util.Arrays;
+ 
+public class Sort {
     public static void main(String[] args) {
-        System.out.println("Bartosz Ziemniak");
-        
-        int[] arr = {5,2,8,1,3};
-
-        // proste sortowanie
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i] > arr[j]){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
-
-        System.out.println("Posortowana tablica:");
-        for(int n : arr){
-            System.out.print(n + " ");
-        }
+        int[] numbers = {5, 2, 8, 1, 9, 3};
+        System.out.println("P2 - Zadanie Jenkins");
+        System.out.println("Tablica przed sortowaniem: " + Arrays.toString(numbers));
+        Arrays.sort(numbers);
+        System.out.println("Tablica po sortowaniu: " + Arrays.toString(numbers));
     }
 }
